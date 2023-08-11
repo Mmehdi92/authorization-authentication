2aae50d2e85d5c314a9ff7dac859a71101c8b9e7
Mehdi Zymberi <m.zymberi@student.avans.nl>
2023-08-11 11:11:17 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,67 @@
+import bodyParser from "body-parser";
+import express, { response } from "express";
+import ejs from "ejs";
+import mongoose from "mongoose";
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
+app.post("/register", async (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  console.log(newUser);
+  try {
+    await newUser.save();
+    console.log("User succesfully added");
+    res.render("secrets");
+  } catch (error) {
+    console.log(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+  console.log(email, password);
+
+  try {
+    const username = await User.findOne({ email: email });
+    
+    if (password === username.password) {
+      res.render("secrets");
+    } else {
+      console.log("handle whatever");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
