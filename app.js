2864632417087e70aa58e9465c90d386a5e60303
Mehdi Zymberi <m.zymberi@student.avans.nl>
2023-08-12 22:27:36 +0200

    Google Auth strategy Level-6

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ import mongoose from "mongoose";
 import session from "express-session";
 import passport from "passport";
 import passportLocalMonogoose from "passport-local-mongoose";
+import GoogleStrategy from "passport-google-oauth20";
+import findOrCreate from "mongoose-findorcreate";
+
 
 const app = express();
 
@@ -24,24 +27,71 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      userProfileUrl: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
+
 mongoose.connect(process.env.DBCONNECTION);
 
 const userSchema = new mongoose.Schema({
+  googleId: String,
   email: String,
   password: String,
 });
 
 userSchema.plugin(passportLocalMonogoose);
+userSchema.plugin(findOrCreate);
+
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, {
+      id: user.id,
+      username: user.username,
+      picture: user.picture,
+    });
+  });
+});
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
 
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect secret.
+    res.redirect("/secrets");
+  }
+);
 
 app.get("/secrets", (req, res) => {
   if (req.isAuthenticated()) {
@@ -56,14 +106,13 @@ app.get("/login", (req, res) => {
 });
 
 app.get("/logout", (req, res) => {
-  req.logout((err)=>{
-    if(err){
-      console.log(err)
-    }else{
-      res.redirect('/')
-      }
+  req.logout((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/");
+    }
   });
-  
 });
 
 app.get("/register", (req, res) => {
